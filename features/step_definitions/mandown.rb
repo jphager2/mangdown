327a00cdd1ffa8b8c817c386d68a1c159a7f3edc
John Hager <jphager2@gmail.com>
2013-09-10 22:07:27 +0200

    first cucumber test passes

--- features/step_definitions/mandown.rb
+++ features/step_definitions/mandown.rb
@@ -1,12 +1,13 @@
+require_relative '../../lib/mandown'
 
 When(/^The program downloads an image$/) do
   uri = 'http://i25.mangareader.net/bleach/537/bleach-4149721.jpg'
   filename = 'Bleach 537 - Page 1'
-  @page = Page.new( uri, filename )
+  @page = Mandown::Page.new( uri, filename )
   @page.download
 end
 
 Then(/^The image will be in the current directory$/) do
   dir = Dir.pwd
-  expect(Dir.globe(dir + '*')).to include(@page.filename)
+  expect(Dir.glob(dir + '/' + '*')).to include(dir + '/' + @page.filename)
 end
