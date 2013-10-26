ccb3ab66c76da8abdc69bed7c271b1b0de2fa636
John Hager <jphager2@gmail.com>
2013-10-26 10:23:13 +0200

    chapter_spec looking prettier

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,10 +13,10 @@ RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
   
-#  config.after(:suite) do
-#    dir = Dir.pwd + '/' + @chapter.name
-#    Dir.rmdir(dir) if Dir.exist?(dir) 
-#  end
+  config.after(:suite) do
+    dir = File.expand_path('../../Bleach 537', __FILE__)
+    Dir.rmdir(dir) if Dir.exist?(dir) 
+  end
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
