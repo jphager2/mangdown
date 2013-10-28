53e677f60c9979b2c1822cc3e7f181f9d7b830ef
John Hager <jphager2@gmail.com>
2013-10-28 23:06:26 +0100

    tests passing, but need to be refactored
    need to use a stub so the popular manga pages are only downloaded once
    with open uri and then saved to a file to reduce the time for the test.
    also once that is done use before(:each) hooks for the tests for
    creating the PopularManga objects.

--- lib/mandown/popular.rb
+++ lib/mandown/popular.rb
@@ -1,10 +1,51 @@
 module Mandown
   class PopularManga
-		attr_reader :uri, :mangas_list
+		attr_reader :uri, :mangas_list, :mangas
 
 		def initialize(uri, num_mangas)
-			@uri = uri
+			@uri = uri #uri = 'http://www.mangareader.net/popular/'
+		  @num_mangas = num_mangas
 			@mangas_list = []
+			@mangas = []
+
+			@root = 'http://www.mangareader.net'
+
+			get_mangas_list
+		end
+
+		def get_manga(index)
+      uri, name = @mangas_list[index - 1]
+			unless @mangas.find {|manga| (manga.name == name) or (manga.uri == uri)}
+        @mangas << Manga.new( uri, name )
+		  else
+				nil
+		  end
+		end
+
+		def get_mangas_list
+      (@num_mangas / 30.0).ceil.times do |time|
+        get_pop_page_manga(time).each { |manga| @mangas_list << manga }
+			end
+			@doc = "Nokogiri::HTML::Document"
+		end
+
+		def get_doc(uri)
+      @doc = Nokogiri::HTML(open(uri))
+		end
+		
+		def get_pop_page_manga(time)
+			num = 30 * (time - 1)
+			page = @uri + '/' + num.to_s
+			get_doc(page)
+      
+			last = (@num_mangas > 30) ? 30 : @num_mangas
+			@num_mangas -= 30
+
+			get_manga_on_page[0..(last - 1)]
+		end
+
+		def get_manga_on_page
+      @doc.css('h3 a').map {|a| [@root + a['href'], a.text]}
 		end
 	end
 end
--- spec/mandown/popular_spec.rb
+++ spec/mandown/popular_spec.rb
@@ -3,57 +3,61 @@ require_relative '../spec_helper'
 module Mandown
   describe PopularManga do
     before(:all) do
-      @list = [20, 50, 90]
-      @num = 0
     end
 
     context "when initialized" do
       before(:each) do 
-        @pop = PopularManga.new('http://www.mangareader.net/popular',@list[@num])
-        @num += 1
       end
 
       context "with top 20" do
         it "should have a manga list with top 20 mangas" do
-          expect(@pop.mangas_list.length).to eq(20)
+          @pop2 = PopularManga.new('http://www.mangareader.net/popular', 20)
+          expect(@pop2.mangas_list.length).to eq(20)
         end
       end
 
       context "with top 50" do
         it "should have a manga list with top 50 mangas" do
-          expect(@pop.mangas_list.length).to eq(50)
+          @pop3 = PopularManga.new('http://www.mangareader.net/popular', 50)
+          expect(@pop3.mangas_list.length).to eq(50)
         end
       end
 
       context "with top 90" do
         it "should have a manga list with top 90 mangas" do
-          expect(@pop.mangas_list.length).to eq(90)
+          @pop4 = PopularManga.new('http://www.mangareader.net/popular', 90)
+          expect(@pop4.mangas_list.length).to eq(90)
         end
       end
  
       it "should have an empty mangas array" do
-        expect(@pop.mangas).to be_empty
+       @pop = PopularManga.new('http://www.mangareader.net/popular', 20)
+       expect(@pop.mangas).to be_empty
       end
     end
 
     context "when get_manga is called" do
       it "should have 1 manga in mangas" do
+        @pop = PopularManga.new('http://www.mangareader.net/popular', 20)
         @pop.get_manga(1)
         expect(@pop.mangas.length).to eq(1)
       end
 
       it "should have 2 mangas in mangas" do
+        @pop = PopularManga.new('http://www.mangareader.net/popular', 20)
         @pop.get_manga(1)
         @pop.get_manga(2)
-        expect(@pop.mangas.length).to eq(1)
+        expect(@pop.mangas.length).to eq(2)
       end
 
       it "should have Bleach in mangas" do
+        @pop = PopularManga.new('http://www.mangareader.net/popular', 20)
         @pop.get_manga(1)
         expect(@pop.mangas.length).to eq(1)
       end
 
       it "should only get manga once" do
+        @pop = PopularManga.new('http://www.mangareader.net/popular', 20)
         @pop.get_manga(1)
         @pop.get_manga(1)
         expect(@pop.mangas.length).to eq(1)
