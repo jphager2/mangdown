82e4a645f4143b5053fdfc7063ab61a400e74662
John Hager <jphager2@gmail.com>
2014-02-13 11:07:52 +0100

    added line for if run in irb to give hint for help

--- lib/mandown.rb
+++ lib/mandown.rb
@@ -14,3 +14,6 @@ require_relative 'mandown/cbz'
 require_relative 'mandown/commands'
 require_relative 'mandown/mandown_hash'
 
+unless $0 == __FILE__
+  puts '*** Use "M.help" for commands ***'
+end
