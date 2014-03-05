e060b9bbec714ed036e4cadf433390b9fb3d60ef
jphager2 <jphager2@gmail.com>
2014-03-05 13:20:05 +0100

    added comments

--- lib/mangdown/page.rb
+++ lib/mangdown/page.rb
@@ -3,10 +3,15 @@ module Mangdown
     attr_reader :filename, :uri
 
     def initialize( uri, filename )
+
+      # this will probably become a problem when adding different
+      # Manga sites which may use different file types than .jpg
       @filename = filename + '.jpg'
       @uri = uri
     end
 
+    # this method should probably be moved somewhere else because
+    # pages, chapters and mangas can all be "downloaded"
     def download
       File.open(@filename, 'wb') do |file|
         file.write(open(URI.encode(@uri, '[]')).read)
