db41a6c096aa4350a119a988ffa76cdbe8514198
jphager2 <jphager2@gmail.com>
2014-03-05 14:13:50 +0100

    done refactoring chapter for now

--- lib/mangdown/chapter.rb
+++ lib/mangdown/chapter.rb
@@ -2,19 +2,20 @@ module Mangdown
   class Chapter
     include ::Mangdown::Tools
 
-    attr_reader :name, :pages, :uri, :num_pages
+    attr_reader :name, :pages, :uri
     
     def initialize( uri, name )
       @uri = uri
       @name = name
       @pages = []
-      @root = ::Mangdown::Tools.get_root(@uri)
 
       get_pages
     end
 
+    # download should be in its own module
     def download
       start_dir = Dir.pwd 
+
       Dir.mkdir(@name) unless Dir.exists?(@name)
       Dir.chdir(@name)
       
@@ -23,34 +24,37 @@ module Mangdown
       Dir.chdir(start_dir)
     end
 
-    def get_pages
-      uri = @uri
-      doc = ::Mangdown::Tools.get_doc(uri)
-      
-      get_num_pages(doc).times do
-        page_uri, page_name = get_page(doc) 
-        uri = get_next_uri(doc)
-
-        @pages << Page.new( page_uri, page_name )
+    private
+      def get_pages
+        uri = @uri
         doc = ::Mangdown::Tools.get_doc(uri)
+        
+        get_num_pages(doc).times do
+          page_uri, page_name = get_page(doc) 
+          uri = get_next_uri(doc)
+
+          @pages << Page.new( page_uri, page_name )
+          doc = ::Mangdown::Tools.get_doc(uri)
+        end
       end
-    end
   end
 
   class MRChapter < Chapter
-    def get_page(doc)
-      image = doc.css('img')[0]
-      [image['src'], image['alt']]
-    end
+    private
+      def get_page(doc)
+        image = doc.css('img')[0]
+        [image['src'], image['alt']]
+      end
 
-    def get_next_uri(doc)
-      #root url + the href of the link to the next page
-      ::Mangdown::Tools.get_root(@uri) + 
-        doc.css('div#imgholder a')[0]['href']
-    end
+      def get_next_uri(doc)
+        #root url + the href of the link to the next page
+        ::Mangdown::Tools.get_root(@uri) + 
+          doc.css('div#imgholder a')[0]['href']
+      end
 
-    def get_num_pages(doc)
-      doc.css('select')[1].children.length
-    end
+      def get_num_pages(doc)
+        # the select is a dropdown menu of chapter pages
+        doc.css('select')[1].children.length
+      end
   end
 end
