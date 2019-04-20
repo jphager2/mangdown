4510df2eb6755286d3878d455ae3bd205ec93bfa
jphager2 <jphager2@gmail.com>
2019-04-20 22:05:12 +0200

    Bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# v0.20.3
+
+  * Fix manga name slug
+
 # v0.20.2
 
   * Fix manga name slug
--- lib/mangdown/version.rb
+++ lib/mangdown/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module Mangdown
-  VERSION = '0.20.2'
+  VERSION = '0.20.3'
 end
